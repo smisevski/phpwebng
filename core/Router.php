f846933ec3540d50e5184c45a3b07ddceb8c035e
Saso Misevski <saso_misevski@yahoo.com>
2019-12-06 12:16:29 +0100

    removed interface approach to helpers
    Removed interface approach to helpers.
    Going toward extended methods and static helpers

--- core/Router.php
+++ core/Router.php
@@ -1,9 +1,10 @@
 <?php
 //use core\lib\interfaces\StringResolveHelper;
 
-class Router implements StringResolveHelper
+class Router  
 {
     private $method;
+    
     private $data;
     
     public static $route_registry = [];
@@ -12,72 +13,99 @@ class Router implements StringResolveHelper
         //
     }
     
+    
     public static function handleURLRequest($url) {
+        
         $url = substr($url, 1);
+        
         $action_trigger = false;
 
-        
+       
         for ($i = 0; $i < sizeof(self::$route_registry); $i++) {
             
             if ($url == self::$route_registry[$i]['name']) {
 
                 $controller_ref = self::$route_registry[$i]['controller'];
+                
                 $method_ref = self::$route_registry[$i]['controller_method'];
+                
                 $controller = new $controller_ref;
+                
                 self::$route_registry[$i]['http_method'] == 'GET' ?
+                
                 $controller->$method_ref() : 
+                
                 $controller->$method_ref($_POST);
                 
                 $action_trigger = true;
+                
                 exit;
+                
             }
         }
         
         if ($action_trigger == false) {
+            
             throw new Exception('Route not registered: '.$url);
+            
         }
     }
+    
     
     public static function get($url, $action) {
       
         $route_spec['name'] = $url;
+        
         $route_spec['http_method'] = 'GET';
             
         $action_parts = explode('@', $action);
+        
         $route_spec['controller'] = $action_parts[0];
+        
         $route_spec['controller_method'] = $action_parts[1];          
  
         self::$route_registry[] = $route_spec;
            
     }
+    
 
     public static function post($url, $action) {
         
         $route_spec['name'] = $url;
+        
         $route_spec['http_method'] = 'POST';
         
         $action_parts = explode('@', $action);
+        
         $route_spec['controller'] = $action_parts[0];
+        
         $route_spec['controller_method'] = $action_parts[1];
         
         self::$route_registry[] = $route_spec;
         
     }
     
+    
 
-    public function resolveURLparams($url) {
+    private function __resolveURLparams($url) {
         
         if(isset($url) && $url != '') {
             
             $url_parts = explode('/', $url);
+            
             $return_params = [];
+            
             ddie($url_parts);
+            
             if (sizeof($url_parts) > 1) {
                 // do the parameters handling options. send to action
                 //
                 for ($i = 0; $i < sizeof($url_parts); $i++) {
+                    
                     $return_params[$$url_parts[$i]] = $url_parts[$i];
+                    
                 }
+                
                 ddie($return_params);
                 
             } else {
--- index.php
+++ index.php
@@ -1,41 +1,38 @@
 <?php
 
-
 require_once('./core/loader.php');
 require_once('./routing/routes.php');
 
 //session_start();
 
 $url = isset($_SERVER['PATH_INFO']) ?
+
        $_SERVER['PATH_INFO'] :
+       
        (isset($_SERVER['ORIG_PATH_INFO']) ? $_SERVER['ORIG_PATH_INFO'] : '');
 
 $url_one = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
-//ini_set('display_errors', 1);
-//
-//error_reporting(E_ALL);
+
 Router::handleURLRequest($url);
-// ddie($url);
-// Router::test($url_one);
-// Router::post($url_one);
+
 ?>
 
-<html>
-<head>
-    <title>TEST</title>
-</head>
-<body>
-<div>
-    <hr>
-    <form method="post" action="post_test">
-        <label>POST METHOD ROUTER TEST</label><br>
-        <input type="text" name="field"><br>
-        <input type="submit" value="Submit Action">
-    </form>
-    <form method="get" action="test">
-    <label>GET METHOD ROUTER TEST</label><br>
-    <input type="submit" value="GET Action">
-    </form>
-</div>
-</body>
-</html>
+<!-- <html> -->
+<!-- <head> -->
+<!--     <title>TEST</title> -->
+<!-- </head> -->
+<!-- <body> -->
+<!-- <div> -->
+<!--     <hr> -->
+<!--     <form method="post" action="post_test"> -->
+<!--         <label>POST METHOD ROUTER TEST</label><br> -->
+<!--         <input type="text" name="field"><br> -->
+<!--         <input type="submit" value="Submit Action"> -->
+<!--     </form> -->
+<!--     <form method="get" action="test"> -->
+<!--     <label>GET METHOD ROUTER TEST</label><br> -->
+<!--     <input type="submit" value="GET Action"> -->
+<!--     </form> -->
+<!-- </div> -->
+<!-- </body> -->
+<!-- </html> -->
